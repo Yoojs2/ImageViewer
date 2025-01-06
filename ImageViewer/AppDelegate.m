8ea7a58aac39c554402e7762db2dc9ae55d9bb92
유재성 <yjsyjs@yujaeseong-ui-MacBookPro.local>
2025-01-06 18:45:10 +0900

    zoom in & out

--- ImageViewer/AppDelegate.m
+++ ImageViewer/AppDelegate.m
@@ -10,12 +10,16 @@
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
+@property CGFloat currentScale; // 현재 이미지 스케일 저장
+
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.currentScale = 1.0;
 }
 
 
@@ -28,11 +32,40 @@
     return YES;
 }
 
+- (void)zoomImageBy:(CGFloat)scaleFactor{
+    //현재 스케일 업데이트
+    self.currentScale *= scaleFactor;
+    
+    //이미지 뷰의 현재 이미지 가져오기
+    NSImage *image = self.imageViewer.image;
+    if(!image)
+    {
+        self.currentScale = 1.0; //이미지 로드 시 초기 스케일로 설정
+        [self.imageViewer setImage:image];
+        [self.imageViewer setFrameSize:image.size]; //이미지 뷰 크기 초기화
+        
+        NSLog(@"이미지가 없습니다."ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ);
+        return;
+    }
+    
+    NSSize newSize = NSMakeSize(image.size.width * self.currentScale, image.size.height * self.currentScale);
+    
+    //이미지 뷰의 프레임 크기 조정
+    [self.imageViewer setFrameSize:newSize];
+    [self.imageViewer setImageScaling:NSImageScaleAxesIndependently]; //이미지 스케일 방식 설정
+}
+
+
+
+
 
 - (IBAction)btnZoomOut:(id)sender {
+    
+    [self zoomImageBy:0.8];
 }
 
 - (IBAction)btnZoomIn:(id)sender {
+    [self zoomImageBy:1.2];
 }
 
 - (IBAction)onBtnImageOpen:(id)sender {
